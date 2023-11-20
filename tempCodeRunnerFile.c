6b7c1eb0134f305e6ecbccc71582e699f2056855
dpp0900 <_dpp@kakao.com>
2023-11-20 12:10:52 +0900

    bead

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -20,46 +20,15 @@ typedef struct {
     int stanza_count;
 } Poem;
 
-char* regex(char* pattern, char* str){
-    int ret;
-    char pattern[128], str[128];
-
-    int cflags = REG_EXTENDED;
-    regex_t reg;
-
-    printf("regexp(%s),string(%s)\n", pattern, str);
-    // reg compile
-    ret = regcomp(&reg, pattern, cflags);
-    if (ret != 0) {
-            char errStr[128];
-            regerror(ret, &reg, errStr, sizeof(errStr));
-            printf("regcomp error(%s)\n", errStr);
-            exit(1);
-    }
-
-    int eflags = 0, len;
-    int offset = 0, cnt = 1;
-    // pattern matching
-    regmatch_t pmatch;
-
-    while((ret = regexec(&reg, str+offset, 1, &pmatch, eflags))== 0)
-    {
-            len = pmatch.rm_eo - pmatch.rm_so;
-            printf("matched string(%d:%.*s)\n",
-                    cnt, len, str+offset+pmatch.rm_so);
-            offset += pmatch.rm_eo;
-            cnt++;
-    }
-    regfree(&reg);
-    return 0;
-}
-
-
 Poem* initPoem(char* context){
     Poem* poem = (Poem*)malloc(sizeof(Poem));
     poem->title = (char*)malloc(sizeof(char)*100);
     int cur = 0;
-    while (context[cur] != '\n'){                                   
+    while (context+cur != '\n'){
+        if (context[cur] == '\n'){
+            break;
+        }
+        printf("%c", context[cur]);
         poem->title[cur] = context[cur];
         cur++;
     }
